465f577895a0025e17b674cb652505dadd228e4c
DominikAlberski <dominik.alberski@gmail.com>
2017-09-07 13:05:11 +0200

    Iterration E2 Handling Errors

--- db/migrate/20170907102004_combine_items_in_cart.rb
+++ db/migrate/20170907102004_combine_items_in_cart.rb
@@ -4,7 +4,7 @@
 #       sums = cart.line_items.group(:product_id).sum(:quantity)
 #       sums.each do |product_id, quantity|
 #         if quantity > 1
-#           cart.line_items.where(product_id: product_id).delete.all
+#           cart.line_items.where(product_id: product_id).delete.all - w tej linijce literowka
 #           item = cart.line_items.build(product_id: product_id)
 #           item.quantity = quantity
 #           item.save!
