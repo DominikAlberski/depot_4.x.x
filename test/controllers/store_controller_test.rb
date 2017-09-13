e6e44f1da76af2fc2366cbd11b047a13042dd016
Dominik <dominik.alberski@gmail.com>
2017-09-13 13:00:58 +0200

    Iteration F5.1 tests fix

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,7 +6,7 @@ class StoreControllerTest < ActionController::TestCase
     assert_response :success
     assert_select '#columns #side a', minimum: 4
     assert_select '#main .entry', minimum: 3
-    assert_select 'h3', 'My book name'
+    assert_select 'h3', 'Programing Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
     assert_select '#visit_counter', 0
   end
@@ -16,5 +16,11 @@ class StoreControllerTest < ActionController::TestCase
     assert_select '#visit_counter', 0
     5.times { get :index }
     assert_select '#visit_counter', 1
+  end
+
+  test 'markup needed for store.coffee is in place' do
+    get :index
+    assert_select '.store .entry > img', 4
+    assert_select '.entry input[type=submit]', 4
   end
 end
